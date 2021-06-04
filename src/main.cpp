9475bb991cc326e3da907a131874377ec04fd6eb
Divyanshu Shekhar <imdshekhar@gmail.com>
2021-06-04 17:48:25 +0530

    Project structure

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <CLI/CLI.hpp>
+
+int main(int argc, char** argv) {
+    CLI::App app{ "App description" };
+
+    std::string filename = "default";
+    app.add_option("-f,--file", filename, "A help string");
+
+    CLI11_PARSE(app, argc, argv);
+    return 0;
+}
