9f65f5328032a0b6ee4a91996840cd76240a0424
Divyanshu Shekhar <imdshekhar@gmail.com>
2021-06-05 14:15:05 +0530

    main setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
-#include <iostream>
-#include <CLI/CLI.hpp>
+#pragma once
+#include <autogit-cli/Application.hpp>
 
 int main(int argc, char** argv) {
-    CLI::App app{ "AutoGit-CLI - A command-line tool to easily create and remove repositories from GitHub and local system." };
-
-    CLI11_PARSE(app, argc, argv);
+    std::string description = AutoGitCLI::description();
+    AutoGitCLI::Application app{ description,argc,argv };
+    AutoGitCLI_PARSE(app, argc, argv);
+    std::cout << AutoGitCLI::Color::green << "Successfully Parsed" << AutoGitCLI::Color::reset << std::endl;
     return 0;
 }
+
